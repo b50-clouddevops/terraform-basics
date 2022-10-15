7317f8636b1ed1924b23d55d81b10e576e3d5734
mvpc-11232 <mvp2612@hotmail.com>
2022-10-15 07:05:08 +0530

    Auto Push Commit

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -37,6 +37,6 @@ variabl "ex-list" {
 
 # From the above list, you can see that data in list can be of any type indivudually. 
 output "ex-list-op" {
-    value = ""
+    value = "Welcome to ${var.ex-list[2]} Training and this contains  ${var.ex-list[0]} and the trainer name is  ${var.ex-list[2]} "
 }
 
