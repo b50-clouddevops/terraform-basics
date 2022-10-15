71c44b08c90008dcf378dabf2919c1343c3bcfa2
mvpc-11232 <mvp2612@hotmail.com>
2022-10-15 06:53:52 +0530

    Auto Push Commit

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -11,4 +11,6 @@ output "sample-op1" {
 }
 
 
-# A variable can
+# A variable can be accessed with or without ${} only if the variable is called. But in case if it is needed to add with some other strings then we need to use double quotes.data "" "name" {
+  
+}
