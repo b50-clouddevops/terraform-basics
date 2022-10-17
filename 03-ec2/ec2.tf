4d9eaca540e548dc550bf3444c5020abb57ced47
mvpc-11232 <mvp2612@hotmail.com>
2022-10-17 06:50:41 +0530

    Updating the Jenkinsfile Message

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami                       = "ami-05317b7d9de33d0b8"
   instance_type             = "t2.micro"
-  vpc_security_group_ids    = aws_security_group.allow_ssh.???????
+  vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
 
   tags     = {
     Name   = "EC2-Terraform-Instance"
