26ba715b9a22b493ed992680a24ecca29ba84e22
mvpc-11232 <mvp2612@hotmail.com>
2022-10-17 06:25:27 +0530

    Auto Push Commit

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "us-east-1"
+    region      = "us-east-1"
 }
 
 resource "aws_instance" "demo" {
   ami           = "ami-05317b7d9de33d0b8"
   instance_type = "t3.micro"
 
-  tags          = {
+  tags     = {
     Name = "Linux-Terraform"
   }
 }
