979493aaa30ebb2a28e069486feb5978a1842b18
mvpc-11232 <mvp2612@hotmail.com>
2022-10-17 06:47:14 +0530

    Auto Push Commit

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -18,17 +18,16 @@ output "public_ip" {
 
 # Creates security group
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    description      = "SSH from INTERNET"
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
+
   }
 
   egress {
@@ -36,7 +35,6 @@ resource "aws_security_group" "allow_ssh" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
