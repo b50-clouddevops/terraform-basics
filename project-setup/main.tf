4ffb86db8f1c661f6f752ae9525d28bd713da984
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 07:29:57 +0530

    Auto Push Commit

--- project-setup/main.tf
+++ project-setup/main.tf
@@ -3,3 +3,27 @@ module "frontend" {
     COMPONENT    = "frontend"
     APP_VERSION  = "0.0.2"
 }
+
+module "cart" {
+    source       = "./ec2"
+    COMPONENT    = "cart"
+    APP_VERSION  = "0.0.2"
+}
+
+module "catalogue" {
+    source       = "./ec2"
+    COMPONENT    = "catalogue"
+    APP_VERSION  = "0.0.2"
+}
+
+module "frontend" {
+    source       = "./ec2"
+    COMPONENT    = "frontend"
+    APP_VERSION  = "0.0.2"
+}
+
+module "frontend" {
+    source       = "./ec2"
+    COMPONENT    = "frontend"
+    APP_VERSION  = "0.0.2"
+}
