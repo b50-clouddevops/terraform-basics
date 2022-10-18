e51d2c6255a2437450f224530f8bac543ca10134
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 06:48:06 +0530

    Auto Push Commit

--- ex2/ec2/main.tf
+++ ex2/ec2/main.tf
@@ -5,8 +5,8 @@ resource "aws_instance" "demo1" {
 
   provisioner "local-exec" {
     command = <<EOF 
-    cd /home/centos/ansible 
-    ansible-playbook  -i public_ip ??? roboshop.yml
+    cd /home/centos/ansible,
+    ansible-playbook  -i public_ip ??? roboshop.yml -e COMPONENT=mongodb -e ENV=dev -e ansible_user=centos -e ansible_password=DevOps321
 
 EOF 
   }
