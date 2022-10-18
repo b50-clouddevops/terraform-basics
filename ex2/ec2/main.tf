ec82a6503b4466ad460e149832f9ea6bd8c651ca
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 06:52:49 +0530

    Auto Push Commit

--- ex2/ec2/main.tf
+++ ex2/ec2/main.tf
@@ -3,14 +3,7 @@ resource "aws_instance" "demo1" {
   instance_type             = "t2.micro"
   vpc_security_group_ids    = [var.sg]
 
-  provisioner "local-exec" {
-    command = <<EOF 
-    sleep 20,
-    cd /home/centos/ansible,
-    ansible-playbook all -i ${self.public_ip} roboshop.yml -e COMPONENT=mongodb -e ENV=dev -e ansible_user=centos -e ansible_password=DevOps321
 
-EOF 
-  }
 
   tags     = {
     Name   = "EC2-Terraform"
