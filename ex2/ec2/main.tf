cb0013ebaf0586d914e23cfc6d897d74faaa9392
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 06:14:22 +0530

    Auto Push Commit

--- ex2/ec2/main.tf
+++ ex2/ec2/main.tf
@@ -1,9 +1,12 @@
 resource "aws_instance" "demo1" {
   ami                       = "ami-05317b7d9de33d0b8"
   instance_type             = "t2.micro"
-  vpc_security_group_ids    = [aws_security_group.allows_ssh.id]
+  vpc_security_group_ids    = [var.sg]
 
   tags     = {
     Name   = "EC2-Terraform"
   }
 }
+
+
+variable "sg" {}
