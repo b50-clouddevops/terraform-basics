a1fead09d5adbe3515e85b8a4af410910303c85b
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 07:17:46 +0530

    test

--- project-setup/provider.tf
+++ project-setup/provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "b50-tf-state-bucket"
+    key    = "project/env/terrafom.tfstate"
+    region = "us-east-1"
+   }
+}
+
 provider "aws" {
-    value = "us-east-1"
+    region = "us-east-1"
 }
