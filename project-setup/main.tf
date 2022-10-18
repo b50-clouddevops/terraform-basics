462e6645def909b2106b73f5dfd6488d6f7a41e7
mvpc-11232 <mvp2612@hotmail.com>
2022-10-18 20:54:30 +0530

    My first push

--- project-setup/main.tf
+++ project-setup/main.tf
@@ -4,26 +4,26 @@ module "frontend" {
     APP_VERSION  = "0.0.2"
 }
 
-module "cart" {
-    source       = "./ec2"
-    COMPONENT    = "cart"
-    APP_VERSION  = "0.0.2"
-}
+# module "cart" {
+#     source       = "./ec2"
+#     COMPONENT    = "cart"
+#     APP_VERSION  = "0.0.2"
+# }
 
-module "catalogue" {
-    source       = "./ec2"
-    COMPONENT    = "catalogue"
-    APP_VERSION  = "0.0.2"
-}
+# module "catalogue" {
+#     source       = "./ec2"
+#     COMPONENT    = "catalogue"
+#     APP_VERSION  = "0.0.2"
+# }
 
-module "payment" {
-    source       = "./ec2"
-    COMPONENT    = "payment"
-    APP_VERSION  = "0.0.2"
-}
+# module "payment" {
+#     source       = "./ec2"
+#     COMPONENT    = "payment"
+#     APP_VERSION  = "0.0.2"
+# }
 
-module "shipping" {
-    source       = "./ec2"
-    COMPONENT    = "shipping"
-    APP_VERSION  = "0.0.2"
-}
+# module "shipping" {
+#     source       = "./ec2"
+#     COMPONENT    = "shipping"
+#     APP_VERSION  = "0.0.2"
+# }
