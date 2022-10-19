b11e7286ebddae010904c420298d72a63b20a019
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 06:23:15 +0530

    Auto Push Commit

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -22,6 +22,5 @@ resource "aws_spot_instance_request" "spot_worker" {
       "ansible-pull -U https://github.com/b50-clouddevops/ansible.git -e COMPONENT=${var.COMPONENT} -e ENV=dev -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
     ]
   }
-
 }
 
