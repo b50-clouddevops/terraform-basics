8265a6da7940caca364aeca00f01d657b238ee0e
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 06:07:06 +0530

    Auto Push Commit

--- project-setup/vars.tf
+++ project-setup/vars.tf
@@ -3,7 +3,17 @@
 variable "ALL_COMPONENTS" {
     default = {
         mongodb = {
-            app_version = ""
+            app_version = "null"
+        },
+        mysql = {
+            app_version = "null"
+        },
+        redis = {
+            app_version = "null"
+        },
+        rabbitmq = {
+            app_version = "null"
         }
+
     }
 }
