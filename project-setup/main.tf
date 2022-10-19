62d9752ddb370b00815ab6870dddfe24a5782e51
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 06:12:27 +0530

    Auto Push Commit

--- project-setup/main.tf
+++ project-setup/main.tf
@@ -1,6 +1,6 @@
 module "frontend" {
     for_each     = var.ALL_COMPONENTS
     source       = "./ec2"
-    COMPONENT    = 
-    APP_VERSION  = "0.0.2"
+    COMPONENT    = each.key  # declarative way of running loops
+    APP_VERSION  = each.value.app_version
 }
