1b4f0464cdbfcfa7693fd5bb9e7dc28255e6f0c9
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 06:08:01 +0530

    Auto Push Commit

--- project-setup/vars.tf
+++ project-setup/vars.tf
@@ -13,7 +13,25 @@ variable "ALL_COMPONENTS" {
         },
         rabbitmq = {
             app_version = "null"
-        }
+        },
+        catalogue = {
+            app_version = "null"
+        },
+        cart = {
+            app_version = "null"
+        },
+        user = {
+            app_version = "null"
+        },
+        shipping = {
+            app_version = "null"
+        },
+        payment = {
+            app_version = "null"
+        },
+        frontend = {
+            app_version = "null"
+        },
 
     }
 }
