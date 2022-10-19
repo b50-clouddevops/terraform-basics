320ce536713c96604a4496c10d5fc854854e76ce
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 06:29:31 +0530

    My first push

--- project-setup/ec2/sg.tf
+++ project-setup/ec2/sg.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["172.31.0.0/16"]
   }
 
   egress {
