da190212d60c33ebeaea7e619afdaa50a4671f5e
mvpc-11232 <mvp2612@hotmail.com>
2022-10-19 06:11:33 +0530

    Auto Push Commit

--- project-setup/main.tf
+++ project-setup/main.tf
@@ -1,53 +1,6 @@
 module "frontend" {
+    for_each     = var.ALL_COMPONENTS
     source       = "./ec2"
-    COMPONENT    = "frontend"
-    APP_VERSION  = "0.0.2"
-}
-
-module "cart" {
-    source       = "./ec2"
-    COMPONENT    = "cart"
-    APP_VERSION  = "0.0.2"
-}
-
-module "catalogue" {
-    source       = "./ec2"
-    COMPONENT    = "catalogue"
-    APP_VERSION  = "0.1.0"
-}
-
-module "payment" {
-    source       = "./ec2"
-    COMPONENT    = "payment"
-    APP_VERSION  = "0.0.2"
-}
-
-module "shipping" {
-    source       = "./ec2"
-    COMPONENT    = "shipping"
-    APP_VERSION  = "0.0.2"
-}
-
-module "mysql" {
-    source       = "./ec2"
-    COMPONENT    = "mysql"
-    APP_VERSION  = "0.0.2"
-}
-
-module "redis" {
-    source       = "./ec2"
-    COMPONENT    = "redis"
-    APP_VERSION  = "0.0.2"
-}
-
-module "mongodb" {
-    source       = "./ec2"
-    COMPONENT    = "rabbitmq"
-    APP_VERSION  = "0.0.2"
-}
-
-module "rabbitmq" {
-    source       = "./ec2"
-    COMPONENT    = "rabbitmq"
+    COMPONENT    = 
     APP_VERSION  = "0.0.2"
 }
